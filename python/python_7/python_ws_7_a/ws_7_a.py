d522960e0d74d3b5cf803268ec51a54ce4bbce2d
leechangho <ckdgh6589@naver.com>
2024-01-22 20:07:52 +0900

    파이썬7일차 실습완료

---
+++ python/python_7/python_ws_7_a/ws_7_a.py
@@ -0,0 +1,7 @@
+# 아래에 코드를 작성하시오.
+my_lsit = [1, 2, 3]
+my_dict = {'A': 1, 'B': 2, 'C': 3}
+print(dir(my_lsit),'\n')
+print(dir(my_dict))
+print(help(list))
+print(help(dict))
---
+++ python/python_7/python_ws_7_b/ws_7_b.py
@@ -0,0 +1,17 @@
+# 아래에 코드를 작성하시오.
+class Myth:
+    type_of_myth = 0
+    def __init__(self,name):
+        self.name = name
+        Myth.type_of_myth+=1
+
+    @staticmethod
+    def description():
+        return '신화는 한 나라 혹은 한 민족으로부터 전승되어 오는 예로부터 섬기는 신을 둘러싼 이야기를 뜻한다.'
+    
+dangun = Myth('dangun')
+greek =Myth('greek & rome')
+print(dangun.name)
+print(greek.name)
+print(f'현재까지 생성된 신화 수 : {Myth.type_of_myth}')
+print(Myth.description())
